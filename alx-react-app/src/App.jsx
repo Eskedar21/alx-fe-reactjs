ade021ee7dffb6d5e5df1989f107b3340ada447e
Eskedar Tesfaye <eskedartesfaye31@gmail.com>
2024-08-11 00:04:38 +0300

    multiple components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,11 @@
+
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 
@@ -12,7 +16,12 @@ function App() {
     <>
       <div>
         <WelcomeMessage/>
-        <a href="https://vitejs.dev" target="_blank">
+        <Header/>
+        <MainContent/>
+        <Footer/>
+      </div>
+      
+        {/* <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
@@ -30,7 +39,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }

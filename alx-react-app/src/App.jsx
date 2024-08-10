73cc27761fb82f82d24e507b68d751683c9efc7f
Eskedar Tesfaye <eskedartesfaye31@gmail.com>
2024-08-11 00:16:49 +0300

    props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
+import UserProfile from './components/UserProfile'
 import Footer from './components/Footer'
 import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
@@ -18,6 +19,7 @@ function App() {
         <WelcomeMessage />
         <Header />
         <MainContent />
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
         <Footer />
       </div>
 

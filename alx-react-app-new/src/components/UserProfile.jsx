5e068d4d203b22b8d07eb76476cc9426037d2c61
Eskedar Tesfaye <eskedartesfaye31@gmail.com>
2024-08-17 16:52:04 +0300

    blue

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ function UserProfile (props) {
  return (
           <div style={{ border: '5px solid #3c1780', padding: '10px', margin: '10px', borderRadius: '10px'  }}>
             <h2 style={{color: '#3c1780', fontSize: '50px', margin: '10px'}}>{props.name}</h2>
-            <p style={{color: '#3c1780', fontSize: '18px'}}>Age: {props.age}</p>
+            <p style={{color: '#3c1780', fontSize: '18px'}}> <span style={{color: 'blue'}}> Age: {props.age} </span></p>
             <p style={{color: '#3c1780', margin: '10px'}}>Bio: {props.bio}</p>
           </div>
         );

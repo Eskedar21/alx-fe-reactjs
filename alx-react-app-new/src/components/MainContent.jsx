4c47abfec427f20a4dba772aaa1f478ad9095e96
Eskedar Tesfaye <eskedartesfaye31@gmail.com>
2024-08-17 16:54:38 +0300

    love

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -2,7 +2,7 @@ function MainContent (){
    
    return(
     <main>
-    <p style={{color: '#3c1780', fontWeight: 'bold', fontSize: '20px'}}>I loveto visit New York, Paris, and Tokyo.</p>
+    <p style={{color: '#3c1780', fontWeight: 'bold', fontSize: '20px'}}>I love to visit New York, Paris, and Tokyo.</p>
   </main>
    )
 }

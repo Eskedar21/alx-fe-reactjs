c5368e5c153fcc49e1dab096a5b863c7b8d35a05
Eskedar Tesfaye <eskedartesfaye31@gmail.com>
2024-08-18 23:42:16 +0300

    style

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav style={{ display: 'flex', gap: '20px', backgroundColor: 'grey', color: 'gold'}}>
+    <nav style={{ display: 'flex', gap: '20px', backgroundColor: 'grey', color: 'gold', justifyContent: 'center', alignItems: 'center'}}>
         <Link to={"/"}> Home </Link>
         <Link to={"/about"}> About </Link>
         <Link to={"/services"}> Services </Link>

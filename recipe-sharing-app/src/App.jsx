983fc353e689c953f5d7fe74d89fe122a7f07a5e
Eskedar Tesfaye <eskedartesfaye31@gmail.com>
2024-08-25 15:53:34 +0300

    space removed

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,5 +1,5 @@
 import AddRecipeForm from './components/AddRecipeForm'
-import RecipeList from './components/RecipeList '
+import RecipeList from './components/RecipeList'
 import './App.css'
 
 function App() {

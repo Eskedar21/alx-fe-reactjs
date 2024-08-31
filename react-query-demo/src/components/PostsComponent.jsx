75c95519befdf6bc66c5ce66069875215bb13cac
Eskedar Tesfaye <eskedartesfaye31@gmail.com>
2024-08-31 19:25:21 +0300

    reactQuery-application

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,16 +1,16 @@
 import { useQuery } from "react-query";
 
-const fetcher = async () => {
+const fetchPosts = async () => {
    const res = await fetch ( 'https://jsonplaceholder.typicode.com/posts');
    return res.json();
 }
 
 const PostsComponent = () => {
 
-const {data, error, isLoading} = useQuery('fetcher', fetcher);
+const {data, isError, isLoading} = useQuery('posts', fetchPosts);
 
  if(isLoading) return <div>Loading...</div>;
- if(error) return <div>Error loading data</div>;
+ if(isError) return <div>Error loading data</div>;
 
   return (
     <div>

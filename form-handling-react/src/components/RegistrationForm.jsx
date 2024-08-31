e888de7b8fd8df0a6cf90d7b2c0ecdebb8eeb7e0
Eskedar Tesfaye <eskedartesfaye31@gmail.com>
2024-08-31 15:30:16 +0300

    controlled-form

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -2,36 +2,51 @@ import { useState } from "react";
 
 const RegistrationForm = () => {
 
-    const [formData, setFormData] = useState(
-        {
-            userName: '',
-            email: '',
-            password: ''
-        }
-    )
-
-    const handleChange = (e) => {
-        const {name, value} = e.target;
-        setFormData(prevValue => ({...prevValue, [name]: value}))
-
-    }
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
     const handleSubmit = (e) => {
-       e.preventDefault();
-       console.log("Form submitted",formData);
-       setFormData({userName: '', email: '', password: ''})
+        e.preventDefault();
+        console.log("Form submitted", { username, email, password });
+        
+        // Reset form fields
+        setUsername('');
+        setEmail('');
+        setPassword('');
     }
 
-  return (
-    <div>
-       <form onSubmit={handleSubmit} style={{display: 'flex', flexDirection: 'column', gap: '15px'}}>
-         <input type="text" name="userName" value={formData.userName}  required   onChange={handleChange} style={{width: '300px'}}/>
-        <input type="emal" name="email" value={formData.email} required onChange={handleChange} style={{width: '300px'}}/>
-         <input type="password" name="password" value={formData.password} required onChange={handleChange} style={{width: '300px'}}/>
-         <button type="submit"> Submit </button>
-       </form>
-    </div>
-  )
+    return (
+        <div>
+            <form onSubmit={handleSubmit} style={{display: 'flex', flexDirection: 'column', gap: '15px'}}>
+                <input 
+                    type="text" 
+                    name="username" 
+                    value={username} 
+                    required 
+                    onChange={(e) => setUsername(e.target.value)} 
+                    style={{width: '300px'}}
+                />
+                <input 
+                    type="email" 
+                    name="email" 
+                    value={email} 
+                    required 
+                    onChange={(e) => setEmail(e.target.value)} 
+                    style={{width: '300px'}}
+                />
+                <input 
+                    type="password" 
+                    name="password" 
+                    value={password} 
+                    required 
+                    onChange={(e) => setPassword(e.target.value)} 
+                    style={{width: '300px'}}
+                />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
 }
 
 export default RegistrationForm;

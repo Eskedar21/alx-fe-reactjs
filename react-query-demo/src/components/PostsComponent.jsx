3289d802b1898f3d4200eec3f067429818d2f605
Eskedar Tesfaye <eskedartesfaye31@gmail.com>
2024-08-31 19:21:53 +0300

    reactQuery-application

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,27 @@
+import { useQuery } from "react-query";
+
+const fetcher = async () => {
+   const res = await fetch ( 'https://jsonplaceholder.typicode.com/posts');
+   return res.json();
+}
+
+const PostsComponent = () => {
+
+const {data, error, isLoading} = useQuery('fetcher', fetcher);
+
+ if(isLoading) return <div>Loading...</div>;
+ if(error) return <div>Error loading data</div>;
+
+  return (
+    <div>
+        {data.map(item => (
+                <div key={item.id} style={{ marginBottom: '20px', padding: '10px', border: '1px solid #ddd' }}>
+                <h2>{item.title}</h2>
+                <p>{item.body}</p>
+              </div>
+            ))}
+    </div>
+  )
+}
+
+export default PostsComponent;

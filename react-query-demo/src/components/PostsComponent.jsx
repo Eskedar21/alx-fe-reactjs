f85ccd7b43f6ec3a36098fa07cf9dbbb97b185c5
Eskedar Tesfaye <eskedartesfaye31@gmail.com>
2024-08-31 19:49:15 +0300

    reactQuery-application

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -11,7 +11,13 @@ const fetchPosts = async () => {
 
 const PostsComponent = () => {
 
-const {data,isError,error, isLoading} = useQuery('posts', fetchPosts);
+    const { data,isError, error, isLoading, isFetching } = useQuery('posts', fetchPosts, {
+        // Customizing the behavior of React Query with these options:
+        staleTime: 10000, // 10 seconds
+        cacheTime: 300000, // 5 minutes
+        refetchOnWindowFocus: true, // Refetches when window gains focus
+        keepPreviousData: true, // Keeps previous data while new data is being fetched
+      });
 
  if(isLoading) return <div>Loading...</div>;
  if(isError) return <div>Error: {error.message}</div>;

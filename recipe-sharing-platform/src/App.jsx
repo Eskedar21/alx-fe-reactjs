8adfde61686e00b673a4af782c3d5cb298342136
Eskedar Tesfaye <eskedartesfaye31@gmail.com>
2024-09-15 11:09:11 +0300

    AddRecipeForm module

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -9,13 +9,13 @@ function App() {
 
   return (
     <>  
-    
+    <AddRecipeForm/>
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/recipe/:id" element={<RecipeDetail />} />
-        <Route path="//add-recipe" element={<AddRecipeForm/>} />
-         
+        {/* <Route path="//add-recipe" element={<AddRecipeForm/>} />
+          */}
       </Routes>
    </Router>
     </>

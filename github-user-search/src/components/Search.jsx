bff1747b33563644933e5af8dcae6e36fdf862c5
Eskedar Tesfaye <eskedartesfaye31@gmail.com>
2024-09-21 19:07:10 +0300

    appling style

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -35,15 +35,18 @@ const Search = () => {
 
   return (
     <>
-      <h1>GitHub Search App</h1>
-      <form onSubmit={searchProfile}>
+      <h1 className='text-indigo-600    font-bold text-5xl '>GitHub Search App</h1>
+      <form onSubmit={searchProfile} className='max-w-md mx-auto flex flex-col p-7 mt-5 gap-5 font-mono'>
         <input
           type="text"
           onChange={handleChange}
           value={searchTerm}
           placeholder="Enter Your GitHub Username"
+          className=' block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2'
         />
-        <button type="submit">Search</button>
+        <input type="text" placeholder='Enter the location' className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2'/>
+        <input type="text" placeholder='Enter minimum repositories' className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2' />
+        <button type="submit" className='block w-full rounded-md bg-indigo-600 py-1.5 text-white' >Search</button>
       </form>
        {isLoading && <p>Loading...</p>}
       {/* Display error if any */}
@@ -51,15 +54,16 @@ const Search = () => {
 
       {/* Display user data if available */}
       {userData && (
-        <div>
-          <h2>{userData.name}</h2>
+        <div className='max-w-md mx-auto flex flex-col p-5 mt-3 gap-5 font-mono border-spacing-40'>
+          <h2 className='font-mono text-indigo-600 text-3xl'>{userData.name}</h2>
           <img
             src={userData.avatar_url}
             alt={userData.login}
             style={{ width: '100px', borderRadius: '50%' }}
+            className='mx-auto'
           />
           <p>
-            <a href={userData.html_url} target="_blank" rel="noopener noreferrer">
+            <a href={userData.html_url} target="_blank" rel="noopener noreferrer" className='block  w-full   rounded-md bg-indigo-600 py-1.5 text-white' >
               View Profile
             </a>
           </p>
@@ -70,3 +74,9 @@ const Search = () => {
 };
 
 export default Search;
+
+
+
+
+
+

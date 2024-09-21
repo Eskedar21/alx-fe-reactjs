57b67e6d96d0ce41c9633026ff370ed0ea2a8ded
Eskedar Tesfaye <eskedartesfaye31@gmail.com>
2024-09-21 15:57:57 +0300

    API fetch

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -4,17 +4,26 @@ import fetchUserData from '../services/githubService';
 const Search = () => {
 
   const [searchTerm, setSearchTerm] = useState('');
+  const [userData, setUserDate] = useState();
+  const [error, setError] = useState(null); 
 
   const handleChange = (e) => {
     const newVal = e.target.value;
     setSearchTerm(newVal)
   }
 
-  const searchProfile = (e) => {
+  const searchProfile = async(e) => {
     e.preventDefault();
   const trimmedTerm = searchTerm.trim();
   if(trimmedTerm){
-    fetchUserData(searchTerm);
+   try{
+     const data = await fetchUserData(searchTerm);
+     setUserDate(data);
+     setError(null)
+   } catch(err) {
+        setError('User not found');
+        setUserDate(null)
+   }
   }else {
     alert('Please enter a valid username.');
   }
@@ -28,6 +37,18 @@ const Search = () => {
         <input type="text" onChange={handleChange} value={searchTerm}  placeholder='Enter Your GitHub User Name' />
         <button type='submit'>Search</button>
      </form>
+     {error && <p>{error}</p>} 
+     {userData && (
+       <div>
+         <h2>{userData.name}</h2>
+          <img src={userData.avatar_url} alt={userData.login} style={{ width: '100px', borderRadius: '50%' }} />
+          <p>
+            <a href={userData.html_url} target="_blank" rel="noopener noreferrer">
+              View Profile
+            </a>
+          </p>
+        </div>
+     )}
     
     
     </>
--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,16 +1,14 @@
 import axios from 'axios';
 
-const fetchUserData = (username) => {
-  // GET request
-  return axios.get(`https://api.github.com/users/${username}`)  // Use backticks for interpolation
-    .then(response => {
-      console.log(response.data); // Handle success
-      return response.data; // Return the data for further use
-    })
-    .catch(error => {
-      console.error('There was an error!', error); // Handle error
-      throw error; // Rethrow the error if needed
-    });
+const fetchUserData = async (username) => {
+  try {
+    const response = await axios.get(`https://api.github.com/users/${username}`);
+    console.log(response.data); // Handle success
+    return response.data; // Return the data for further use
+  } catch (error) {
+    console.error('There was an error!', error); // Handle error
+    throw error; // Rethrow the error if needed
+  }
 }
 
 export default fetchUserData;
